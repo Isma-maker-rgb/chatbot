ead20cc4d9ab042d282c7f07aa53ed13ac61797f
WhiteStarrr_ <quentinbilou@gmail.com>
2026-01-20 08:55:05 +0100

    ez team diff

--- ProjetChatbot/src/Utilitaire.java
+++ ProjetChatbot/src/Utilitaire.java
@@ -331,6 +331,17 @@ public class Utilitaire {
         // remarque 3 : on aurait pu calculer directement une intersection au lieu d'une fusion et se passer de maxOccurences mais on
         // souhaite pouvoir garder la possibilité d'assouplir par la suite la contrainte sur la présence de l'intégralité
         // des mots de la question dans la réponse }
+        ArrayList<String> motsQuestion = decoupeEnMots(question);
+        ArrayList<Integer> listeFusionnee = new ArrayList<>();
+        int nbMotsNonOutils = 0;
+
+        for(String mot : motsQuestion){
+            if(! existeChaineDicho(motsOutils,mot)) {
+                nbMotsNonOutils++;
+                ArrayList<Integer> sorties = IndexReponses.rechercherSorties(mot);
+                listeFusionnee = fusion(listeFusionnee, sorties);
+            }
+        }
         return new ArrayList<Integer>();
     }
 
